fdd520edc0504cd3bdb3f47c0d2ce738cd70a02a
girl-on-a-string <frenchfries55@protonmail.com>
2025-02-04 13:25:48 -0600

    slides and dots now connected properly. Selected dot 'goes to' proper slide

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import "./styles.scss";
 
+const currentSlide = (n) => { // current selected slide
+    let slides = document.querySelectorAll(".slide");
+    console.log(slides[n - 1]); 
+}
+
+let dots = document.querySelectorAll(".dot");
+dots.forEach((dot) => {
+    dot.addEventListener("click", () => { // for if someone chooses to click on one
+        let selected = parseInt(dot.getAttribute("data-value"))
+        currentSlide(selected);
+        console.log(selected);
+    })
+
+    // for automatic affect
+})
+
+// event listeners
+
+document.getElementById("next").addEventListener("click", () => {
+    
+})
+
+document.getElementById("prev").addEventListener("click", () => {
+
+})
