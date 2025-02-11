6731e5d5c1896f23bb52c17981b905ad8f619971
girl-on-a-string <frenchfries55@protonmail.com>
2025-02-11 10:36:29 -0600

    automatic cycling now works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "./styles.scss";
 
-const currentSlide = (n) => { // current selected slide
-    let strip = document.getElementById("strip");
-    let slides = document.querySelectorAll(".slide");
-    
+let slides = document.querySelectorAll(".slide");
+
+const currentSlide = (n) => { // current selected slide    
     let i;
 
     for (i = 0; i < slides.length; i++) {
@@ -14,31 +13,52 @@ const currentSlide = (n) => { // current selected slide
 
     dots[n - 1].classList.add("active");
 
-    for (i = 0; i < dots.length; i++) {
+    for (i = 0; i < dots.length; i++) { // remove active class from unclicked dot
         if (dots[i] !== dots[n - 1]) {
             dots[i].classList.remove("active");
         }
     }
-
-    // arrow event listeners
-
-    document.getElementById("next").addEventListener("click", () => {
-        
-        console.log(slides[n - 1]); 
-    })
-
-    document.getElementById("prev").addEventListener("click", () => {
-        
-        console.log(slides[n - 1]); 
-    })
 }
 
+// dots
+
 let dots = document.querySelectorAll(".dot");
+
 dots.forEach((dot) => {
+    let selected = parseInt(dot.getAttribute("data-value"))
+
     dot.addEventListener("click", () => { // for if someone chooses to click on one
-        let selected = parseInt(dot.getAttribute("data-value"))
-        // dot.classList.add("active");
         currentSlide(selected);
     })
-
 })
+
+// arrow event listeners
+
+// document.getElementById("next").addEventListener("click", () => {
+//     currentSlide();
+//     console.log(currentSlide())
+// })
+    
+// document.getElementById("prev").addEventListener("click", () => {
+//     currentSlide();
+//     console.log(currentSlide())
+// })
+
+// automatic cycle
+
+const autoCycle = () => {
+    let slideNum = 0
+
+    setInterval(() => {
+        slideNum++
+
+        if (slideNum > slides.length) {
+            slideNum = 1;
+        } 
+        
+        currentSlide(slideNum)
+    }, 5000)
+}
+
+autoCycle()
+
