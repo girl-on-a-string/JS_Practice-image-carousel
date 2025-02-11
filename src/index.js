d2168ce45e357175f2fcc90704838832ed7934ed
girl-on-a-string <frenchfries55@protonmail.com>
2025-02-11 10:38:11 -0600

    formatted with prettier

--- src/index.js
+++ src/index.js
@@ -2,35 +2,38 @@ import "./styles.scss";
 
 let slides = document.querySelectorAll(".slide");
 
-const currentSlide = (n) => { // current selected slide    
-    let i;
+const currentSlide = (n) => {
+  // current selected slide
+  let i;
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[n - 1].style.display = "initial";
+
+  dots[n - 1].classList.add("active");
+
+  for (i = 0; i < dots.length; i++) {
+    // remove active class from unclicked dot
+    if (dots[i] !== dots[n - 1]) {
+      dots[i].classList.remove("active");
     }
-
-    slides[n - 1].style.display = "initial";
-
-    dots[n - 1].classList.add("active");
-
-    for (i = 0; i < dots.length; i++) { // remove active class from unclicked dot
-        if (dots[i] !== dots[n - 1]) {
-            dots[i].classList.remove("active");
-        }
-    }
-}
+  }
+};
 
 // dots
 
 let dots = document.querySelectorAll(".dot");
 
 dots.forEach((dot) => {
-    let selected = parseInt(dot.getAttribute("data-value"))
+  let selected = parseInt(dot.getAttribute("data-value"));
 
-    dot.addEventListener("click", () => { // for if someone chooses to click on one
-        currentSlide(selected);
-    })
-})
+  dot.addEventListener("click", () => {
+    // for if someone chooses to click on one
+    currentSlide(selected);
+  });
+});
 
 // arrow event listeners
 
@@ -38,7 +41,7 @@ dots.forEach((dot) => {
 //     currentSlide();
 //     console.log(currentSlide())
 // })
-    
+
 // document.getElementById("prev").addEventListener("click", () => {
 //     currentSlide();
 //     console.log(currentSlide())
@@ -47,18 +50,17 @@ dots.forEach((dot) => {
 // automatic cycle
 
 const autoCycle = () => {
-    let slideNum = 0
+  let slideNum = 0;
 
-    setInterval(() => {
-        slideNum++
+  setInterval(() => {
+    slideNum++;
 
-        if (slideNum > slides.length) {
-            slideNum = 1;
-        } 
-        
-        currentSlide(slideNum)
-    }, 5000)
-}
+    if (slideNum > slides.length) {
+      slideNum = 1;
+    }
 
-autoCycle()
+    currentSlide(slideNum);
+  }, 5000);
+};
 
+autoCycle();
